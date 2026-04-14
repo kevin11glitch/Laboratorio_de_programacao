48f622d7cd06c031f0c2db44de88429888e9ef2b
Kevin Mendes <kevinmendes765@gmail.com>
2026-04-14 12:58:00 -0300

    lista 03

---
+++ Lista_03/01.c
@@ -0,0 +1,41 @@
+/* 
+01 - Uma opção para substiruir o if...else de forma enxuta e compacta.
+
+02 - Sintaxe: condição ? verdadeiro : falso.
+
+03 - 
+    a) Saida: 1.
+    b) Saida: 100.
+    c) Saida: 2.
+    d) Saida: 6.
+    e) Saida: 5.
+    f) Saida: 8.
+
+04 - a > b ? maior = a : maior = b;
+
+05 - 
+    a)  int n = -5;
+        printf("%s", (n>=0) ? "Positivo" : "Negativo");
+    b)  int idade = 20;
+        char *msg = (idade >= 18) ? "Adulto" : "Não é adulto";
+
+06 - c) if-else.
+
+07 - 
+    a)  (a == b) ? resultado = 0 : ((a > b) ? resultado = 1 : resultado = -1);
+    b)  (x % 2 == 0) ? par = 1 : par = 0;
+    c)  (nota >= 7) ? printf("Aprovado") : printf("Reprovado");
+
+08 - (x % 2 == 0) ? ((x > 10) ? printf("Par e maior que 10") : printf("Par pequeno")) : printf("Impar");
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+
+
+    return 0;
+}
